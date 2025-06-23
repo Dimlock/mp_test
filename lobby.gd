a4441262c6a7201c839c73dfc6b18dffe0e156f1
Dimlock <roshin.artur@yandex.ru>
2025-06-23 03:55:35 +0300

    Кастомный спавнер и разные авторитеты у узлов

--- lobby.gd
+++ lobby.gd
@@ -5,22 +5,19 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	multiplayer_spawner.spawn_function = custom_spawn
+	multiplayer.peer_connected.connect(_on_peer_connected)
 	if multiplayer.is_server():
-		multiplayer.peer_connected.connect(_on_peer_connected)
 		_on_peer_connected(1)
 
-	#if multiplayer.is_server():
-		#for peer in Network.peers:
-			#var new_player = player_in_lobby_scene.instantiate()
-			#players_list.add_child(new_player)
-			#new_player.setup(peer["name"])
-			#new_player.set_multiplayer_authority(peer["id"])
-		
 
 func _on_peer_connected(id):
 	if id != 1:
 		await Network.peer_added
 	var peer = Network.find_peer_by_id(id)
-	var new_player = player_in_lobby_scene.instantiate()
-	players_list.add_child(new_player, true)
-	new_player.setup(peer["name"], id)
+	multiplayer_spawner.spawn(peer)
+
+func custom_spawn(data):
+	var node = player_in_lobby_scene.instantiate()
+	node.setup(data["name"], data["id"])
+	return node
--- player_in_lobby.gd
+++ player_in_lobby.gd
@@ -1,19 +1,22 @@
 extends HBoxContainer
+
+
 @onready var player_name: Label = %PlayerName
 @onready var ready_button: Button = $ReadyButton
 
 var ready_pressed: bool
-var uid:
-	set(value):
-		uid = value
-		if ready_button:
-			ready_button.set_multiplayer_authority(value)
+var player_name_text: String
+var uid: int
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(uid)
 
 func setup(player_name_param, id):
-	player_name.text = player_name_param
+	player_name_text = player_name_param
 	uid = id
 
+func _ready() -> void:
+	player_name.text = player_name_text
 
 func _on_ready_button_pressed() -> void:
 	if not ready_button.is_multiplayer_authority():
@@ -23,9 +26,3 @@ func _on_ready_button_pressed() -> void:
 	else:
 		ready_button.text = "X"
 	ready_pressed = not ready_pressed
-	_on_ready_button_pressed_rpc.rpc_id(1, ready_button.text, ready_pressed)
-
-@rpc("any_peer", "call_local")
-func _on_ready_button_pressed_rpc(text, pressed_status):
-	ready_button.text = text
-	ready_pressed = pressed_status
